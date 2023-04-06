1c09d14f21703f98ed068be6897f1b6375faf140
Drew Showalter <10665739+one19@users.noreply.github.com>
2023-04-06 21:01:04 +1000

    :sparkles: make sword do animation on enemy.

--- scenes/abilities/swordAbility/controller.gd
+++ scenes/abilities/swordAbility/controller.gd
@@ -2,6 +2,8 @@ extends Node
 
 @export var swordAbility: PackedScene
 
+@export var maxRange: float
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	# get_node("Timer")
@@ -10,10 +12,24 @@ func _ready():
 
 func onTimerTimeout():
 	var player = get_tree().get_first_node_in_group("player") as Node2D
-
+	var enemies = get_tree().get_nodes_in_group("enemies")
+	
 	if player == null:
 		return
+	
+	enemies = enemies.filter(func (enemy: Node2D):
+		return enemy.global_position.distance_squared_to(player.global_position) < pow(maxRange, 2)
+	)
+
+	if enemies.size() == 0:
+		return
+		
+	enemies.sort_custom(func (a: Node2D, b: Node2D):
+		var aDistance = a.global_position.distance_squared_to(player.global_position)
+		var bDistance = b.global_position.distance_squared_to(player.global_position)
+		return aDistance < bDistance
+	)
 
 	var swordInstance = swordAbility.instantiate() as Node2D
 	player.get_parent().add_child(swordInstance)
-	swordInstance.global_position = player.global_position
+	swordInstance.global_position = enemies[0].global_position
