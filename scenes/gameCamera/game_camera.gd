0e26ee93f3f88576f246c8affb8a4ff5cb2e2152
Drew Showalter <10665739+one19@users.noreply.github.com>
2023-04-06 16:56:53 +1000

    :sparkles: added camera with lerp.

---
+++ scenes/gameCamera/game_camera.gd
@@ -0,0 +1,26 @@
+extends Camera2D
+
+const SMOOTHING_VALUE = 10
+
+# we could also try not mutating and see if there's a performance hit.
+var target_position = Vector2.ZERO
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	make_current()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	acquireTarget()
+	global_position = global_position.lerp(
+		target_position, 1.0 - exp(-delta * SMOOTHING_VALUE)
+	)
+
+
+func acquireTarget():
+	var playerNodes = get_tree().get_nodes_in_group("player")
+	if playerNodes.size() > 0:
+		var player = playerNodes[0] as Node2D
+		target_position = player.global_position
