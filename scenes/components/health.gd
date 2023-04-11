5a20f67af391bc8d6981926fa829e50897a5be3b
Drew Showalter <10665739+one19@users.noreply.github.com>
2023-04-11 15:33:08 +1000

    :sparkles: create a working health bar.

--- scenes/components/health.gd
+++ scenes/components/health.gd
@@ -2,6 +2,7 @@ extends Node
 class_name Health
 
 signal died
+signal healthChanged(healthPercent: float)
 
 @export var maxHealth: float = 10
 var currentHealth
@@ -11,10 +12,16 @@ func _ready():
 
 func takeDamage(damage: float):
 	currentHealth = max(currentHealth - damage, 0)
+	healthChanged.emit(getPercent()) # must add on healing method too.
 	
 	# something in our death handling is mutating
 	# this deferral waits for death until the spider animation is idle
 	Callable(checkIfDead).call_deferred()
+
+
+func getPercent():
+	return min(currentHealth / maxHealth, 1)
+
 
 func checkIfDead():
 	if currentHealth == 0:
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -4,6 +4,8 @@ const MAX_SPEED = 200
 const ACCELERATION_SMOOTHING = 15
 
 @onready var damageTimer = $DamageIntervalTimer
+@onready var healthBar = $HealthBar
+@onready var health = $Health
 
 var numberOfCollidingBodies = 0
 
@@ -12,9 +14,12 @@ func _ready():
 	$CollisionArea2D.body_entered.connect(onEnemyEntered)
 	$CollisionArea2D.body_exited.connect(onEnemyExited)
 	damageTimer.timeout.connect(handleContinuousDamage)
+	health.healthChanged.connect(onHealthChanged)
+	healthBar.value = health.getPercent() # this is meh, but children are already ready
+# if they weren't we couldn't instantiate the parent, so there'd be nothing to listen to
+# data only flows after everything is made. This is an acceptable solution for dynamic health
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var movementVector = getMovementVector()
 	var direction = movementVector.normalized()
@@ -31,6 +36,15 @@ func getMovementVector():
 	return Vector2(xDirection, yDirection)
 
 
+func handleContinuousDamage():
+	if numberOfCollidingBodies == 0 || !damageTimer.is_stopped():
+		return
+
+	health.takeDamage(1)
+	damageTimer.start()
+	print("health", health.currentHealth)
+
+
 func onEnemyEntered(enemyArea: Node2D):
 	numberOfCollidingBodies += 1
 	handleContinuousDamage()
@@ -38,11 +52,5 @@ func onEnemyEntered(enemyArea: Node2D):
 func onEnemyExited(enemyArea: Node2D):
 	numberOfCollidingBodies -= 1
 
-
-func handleContinuousDamage():
-	if numberOfCollidingBodies == 0 || !damageTimer.is_stopped():
-		return
-
-	$Health.takeDamage(1)
-	damageTimer.start()
-	print("health", $Health.currentHealth)
+func onHealthChanged(healthPercent: float):
+	healthBar.value = healthPercent
