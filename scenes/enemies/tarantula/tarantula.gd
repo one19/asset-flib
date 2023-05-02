fa7053d767b0ca7ff7051f9f9fc2882ab03d3dac
Drew Showalter <10665739+one19@users.noreply.github.com>
2023-05-02 18:18:01 +1000

    :sparkles: add jumping bouncy spider.

--- scenes/enemies/tarantula/tarantula.gd
+++ scenes/enemies/tarantula/tarantula.gd
@@ -1,8 +1,39 @@
 extends CharacterBody2D
 
-@onready var movementHandler = $Velocity
+@onready var movementHandler: VelocityManager = $Velocity
+@onready var animationPlayer: AnimationPlayer = $AnimationPlayer
+@onready var leapTimer: Timer = $LeapTimer
 
+var isJumping = false
+var isPrepping = false
+
+func _ready():
+	leapTimer.connect('timeout', doLeapAnimation)
 
 func _process(_delta):
-	movementHandler.accelerateToPlayer()
+	if isJumping:
+		movementHandler.leap()
+	elif isPrepping:
+		movementHandler.decelerate()
+	else:
+		movementHandler.accelerateToPlayer()
+		movementHandler.rotateAlongDirection(self)
+
 	movementHandler.moveBouncy(self)
+
+
+func setJumping(jumping: bool):
+	isJumping = jumping
+	isPrepping = false
+
+	if jumping == false:
+		leapTimer.start(10)
+
+
+func setPrepping(prepping: bool = true):
+	isPrepping = prepping
+
+
+func doLeapAnimation():
+	leapTimer.stop()
+	animationPlayer.play('leap')
